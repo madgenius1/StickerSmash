dfcaa86ed5bfe03382aafc6e3f70f4df064c604a
ME <nickjuma@outlook.com>
2025-10-30 11:09:56 +0300

    Adding gestures

---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,15 @@
+import { ImageSourcePropType, View } from 'react-native';
+import Animated from 'react-native-reanimated';
+
+type Props = {
+  imageSize: number;
+  stickerSource: ImageSourcePropType;
+};
+
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  return (
+    <View style={{ top: -350 }}>
+      <Animated.Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+    </View>
+  );
+}
